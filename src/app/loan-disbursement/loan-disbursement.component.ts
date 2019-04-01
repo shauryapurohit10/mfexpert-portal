b64b40e1848dbc3ce5e66e1db434c7859a217e77
Shaurya Purohit <shaurya.purohit@sls.ads.valuelabs.net>
2019-04-01 11:51:01 +0530

    loan disburesement changes

--- src/app/loan-disbursement/loan-disbursement.component.ts
+++ src/app/loan-disbursement/loan-disbursement.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Router } from '@angular/router';
-import { FormBuilder} from '@angular/forms';
+import { FormBuilder, FormGroup} from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
 
 @Component({
@@ -18,12 +18,45 @@ export class LoanDisbursementComponent implements OnInit {
   users: any[]
   @Output() buttonClicked = new EventEmitter();
 
+  disbursementForm: FormGroup;
+  submitted = false;
+
   constructor(private router : Router, private formBuilder: FormBuilder,  private http: HttpClient) { }
 
   ngOnInit() {
     this.http.get("http://localhost:3001/api/v1/loandisbursementusers").subscribe((data) => {
     //console.log(data)
     this.users = data['responseMessage'];
+  })
+
+  this.disbursementForm = this.formBuilder.group({
+    member_code: [''],
+    member_name: [''],
+    loan_amount: [''],
+    outstanding_amount: [''],
+    loan_type: ['']
+    });
+  }
+
+  onSubmit() {
+    const loanPayload = {
+      member_code: this.disbursementForm.controls.member_code.value,
+      member_name: this.disbursementForm.controls.member_name.value,
+      loan_amount: this.disbursementForm.controls.loan_amount.value,
+      outstanding_amount: this.disbursementForm.controls.outstanding_amount.value,
+      loan_type: this.disbursementForm.controls.loan_type.value
+     }
+    this.submitted = true;
+
+    let data_success
+    this.http.post("http://localhost:3001/api/v1/loandisbursementdbusers", loanPayload).subscribe((data) => {
+    //console.log(data)
+    data_success = data;
+    if(data_success.responseMessage.length > 0) {
+      alert("Disbursed successfully!");
+    } else {
+      alert("Please try again later!!");
+    }
   })
   }
 
